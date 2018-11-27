90fa7287ef873f2c4d0fcf1d05329b22cf047246
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-26 22:55:19 -0800

    Fixed Testing

--- __tests__/server-test.js
+++ __tests__/server-test.js
@@ -6,12 +6,21 @@ const mockRequest = supertest(server);
 
 describe ('client server', () => {
 
-  it('should respond with a 201 status when routed to the home page', () => {
+  it('should respond with a 200 status when routed to the home page', () => {
     return mockRequest
       .get('/')
       .then(results => {
-        console.log('results', results.status);
-        expect(results.status).toBe(500);
+        expect(results.status).toBe(200);
+      }).catch(err => {
+        console.log('error', err);
+      });
+  });
+
+  it('should respond with a 404 status when routed to a page that does not exist', () => {
+    return mockRequest
+      .get('/foo')
+      .then(results => {
+        expect(results.status).toBe(404);
       }).catch(err => {
         console.log('error', err);
       });
