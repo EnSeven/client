0342c243efba6663aad5cd69366132636c85cf42
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-27 23:14:42 -0800

    Updated server.js file

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 'use strict';
-const io = require('socket.io');
+// const io = require('socket.io');
+var socket = io();
 
 io.connect('localhost://3000');
 
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 'use strict';
 
-const io = require('socket.io')();
+const io = require('socket.io')(3000);
 const sockets = [];
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
   sockets.push(socket);
   console.log('player 1 ready');
   // sockets[socket.id] = socket;
   // socket.on('start', payload) => {
   //   io.broadcast('player 1 ready');
   // }
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
-io.listen(3000);
-console.log('Listening on port 3000');
+console.log('Ready to play on port 3000');
