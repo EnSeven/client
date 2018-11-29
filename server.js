3f6750e57d6e09a413d0b82f8a3c066e783cdb77
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-28 18:30:07 -0800

    Update console logs to make sense

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const sockets = [];
 // when someone connects to the server (nodemon or node server.js)
 io.on('connection', (socket) => {
   sockets.push(socket);
-  // console.log('sockets', sockets);
+  console.log('sockets', sockets);
 
   // when someone connects via node client.js
   socket.on('start', () => {
-    socket.emit('connected', `player ${socket.id} ready`);
+    socket.emit('connected', `Player ${socket.id} ready`);
+    console.log(`Player ${socket.id} has joined the game`);
+
   });
 
   // when someone disconnects via node client.js
   socket.on('disconnect', () => {
-    console.log('user has left the game');
+    socket.removeAllListeners();
+    console.log(`Player ${socket.id} has left the game`);
   });
 
 });
