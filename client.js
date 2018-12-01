caba8efa501afc4cf857ecfac0c4a8b8d9552864
Mike K <m.kermes89@gmail.com>
2018-12-01 14:41:39 -0800

    readline welcome prompt, logic for user to create or login

--- client.js
+++ client.js
@@ -5,19 +5,37 @@ const prompt = require('prompt');
 const socket = io.connect('https://enseven-game-engine.herokuapp.com');
 const readline = require('readline');
 
+//setting readline to read from standard (keyboard) input and output streams
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('>>What\'s your name? ', function(answer){
-  console.log('Hello ' + answer);
+
+// Welcome user. Explain how to use app by setting the rl prompt, and then calling the prompt
+rl.setPrompt('Welcome to N7 Games. To begin type LOGIN to login to an existing account, or type CREATE to create a new account.');
+rl.prompt();
+
+rl.question('LOGIN or CREATE account? ', function(answer){
+  if(answer){
+    switch(answer){
+    case 'LOGIN':
+      // put login function / event here
+      break;
+    case 'CREATE':
+      // put create account function / event here
+      break;
+    //setting the default to kindly remind the user how to not cause errors and asking again
+    default:
+      rl.setPrompt('LOGIN to login, CREATE to create new account. ');
+      rl.prompt();
+      // once we wrap this question in a function, callback this function to run again.
+    }
+  }
   rl.close();
 });
 
-
-
-
+// on connection start the prompt module and collect login / account based on the userSchema (we can and should keep userSchema outside of the function later on)
 socket.on('connected', payload => {
   console.log(payload);
   prompt.start();
