d50a90e724dded4c2ac3922fa3e510772a3d3d84
Brandon Haynes <brandon@Brandons-MBP-2.localdomain>
2018-12-01 15:07:19 -0800

    in progress

--- __tests__/client-test.js
+++ __tests__/client-test.js
@@ -1,64 +1,30 @@
 'use strict';
 
-import test from 'ava';
-import { SocketIO, Server } from 'mock-socket';
+// import { SocketIO, Server } from 'mock-socket';
 
-const io = require('socket.io-client');
+// const io = require('socket.io-client');
+
+// Require the module under test
+
+const ioClient = require('socket.io-client');
+const ioServer = require('socket.io')(3000);
+
+
+const client = require('../client.js');
+
+let urlVariable = 'https://enseven-game-engine.herokuapp.com';
+const socket = io.connect(urlVariable);
+
 describe('client app', () => {
-  // let app, server;
+  it('does not allow objects as a param', () => {
+    let message = hello.sayHello({});
+    expect(message).toBeNull();
+  });
 
-  // beforeAll(done => {
-  //     app = new express();
-  //     server = http.createServer(app);
-  //     server.listen(done);
-  // });
-
-  // afterAll(done => {
-  //     server.close(done);
-  // });
-
-  // it('returns 404 when sent to route that does not exist', async () => {
-  //     const response = await supertest(app).get('/foobar');
-  //     expect(response.status).toBe(404);
-  // });
-
-  class ChatApp {
-    constructor(url) {
-      this.messages = [];
-      this.connection = new io(url);
-      
-      this.connection.on('chat-message', event => {
-        this.messages.push(event.data);
-      });
-    }
-    
-    sendMessage(message) {
-      this.connection.emit('chat-message', message);
-    }
-  }
-   
-  it('ensures that socket.io works', t => {
-    const fakeURL = 'ws://localhost:8080';
-    const mockServer = new Server(fakeURL);
-    
-    window.io = SocketIO;
-    
-    mockServer.on('connection', socket => {
-      socket.on('chat-message', data => {
-        t.is(data, 'test message from app', 'we have intercepted the message and can assert on it');
-        socket.emit('chat-message', 'test message from mock server');
-      });
-    });
-    
-    const app = new ChatApp(fakeURL);
-    app.sendMessage('test message from app');
-    
-    setTimeout(() => {
-      t.is(app.messages.length, 1);
-      t.is(app.messages[0], 'test message from mock server', 'we have subbed our websocket backend');
-      
-      mockServer.stop(t.done);
-    }, 100);
+  it('works when given a word', () => {
+    var message = hello.sayHello('John');
+    var expectedOutput = 'Hello, John';
+    expect(message).toEqual(expectedOutput);
   });
 
 }); 
--- client.js
+++ client.js
@@ -4,53 +4,57 @@ const io = require('socket.io-client');
 const superagent = require('superagent');
 const prompt = require('prompt');
 
-const 
+
 const socket = io.connect('https://enseven-game-engine.herokuapp.com');
 //const socket = io.connect('https://enseven-game-engine.herokuapp.com');
 
-
-socket.on('connected', payload => {
-  console.log(payload);
-  prompt.start();
-  const userSchema = {
-    properties: {
-      username: {
-        pattern: /^[a-zA-Z0-9]+$/,
-        message: 'Name must be only letters and numbers',
-        required: true,
-      },
-      password: {
-        hidden: true,
-        replace: '*',
-        required: true,
-      },
-    },
-  };
+function connectAndPrompt() {
   
-  // Get two properties from the user: email, password
-  prompt.get(userSchema, function (err, result) {
-    // Log the results.
-    //result should be sent to API
-
-    // console log for testing
-    // console.log('results', JSON.stringify(result));
-
-    let newResult = JSON.stringify(result);
-    superagent.post('https://enseven-api-service.herokuapp.com/signup')
-      .send(newResult)
-      .set('Content-Type', 'application/json')
-      .then(data =>{
-
-        // console log for testing
-        // console.log('user data test', data);
-
-        socket.emit('join', data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+  socket.on('connected', payload => {
+    
+    console.log(payload);
+    
+    const userSchema = {
+      properties: {
+        username: {
+          pattern: /^[a-zA-Z0-9]+$/,
+          message: 'Name must be only letters and numbers',
+          required: true,
+        },
+        password: {
+          hidden: true,
+          replace: '*',
+          required: true,
+        },
+      },
+    };
+    
+    prompt.start();
+    // Get two properties from the user: email, password
+    prompt.get(userSchema, function (err, result) {
+      // Log the results.
+      //result should be sent to API
+  
+      // console log for testing
+      // console.log('results', JSON.stringify(result));
+  
+      let newResult = JSON.stringify(result);
+      superagent.post('https://enseven-api-service.herokuapp.com/signup')
+        .send(newResult)
+        .set('Content-Type', 'application/json')
+        .then(data =>{
+  
+          // console log for testing
+          // console.log('user data test', data);
+  
+          socket.emit('join', data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
   });
-});
+}
 
 // sending to sender-client only
 socket.emit('start');
