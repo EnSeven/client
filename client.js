8c3c66b5deb461c9c8f1517c6261364c670370d8
Nicholas Welch <nicholaswelch1989@gmail.com>
2018-12-01 12:08:08 -0800

    adding a testersever

--- client.js
+++ client.js
@@ -49,3 +49,45 @@ socket.on('connected', payload => {
 
 // sending to sender-client only
 socket.emit('start');
+
+//test server to verify input output 
+
+import test from 'ava';
+import { Server } from 'mock-socket';
+
+class ChatApp {
+  constructor(url) {
+    this.messages = [];
+    this.connection = new WebSocket(url);
+
+    this.connection.onmessage = (event) => {
+      this.messages.push(event.data);
+    };
+  }
+
+  sendMessage(message) {
+    this.connection.send(message);
+  }
+}
+
+test.cb('that chat app can be mocked', t => {
+  const fakeURL = 'ws://localhost:8080';
+  const mockServer = new Server(fakeURL);
+
+  mockServer.on('connection', socket => {
+    socket.on('message', data => {
+      t.is(data, 'test message from app', 'we have intercepted the message and can assert on it');
+      socket.send('test message from mock server');
+    });
+  });
+
+  const app = new ChatApp(fakeURL);
+  app.sendMessage('test message from app'); // NOTE: this line creates a micro task
+
+  // NOTE: this timeout is for creating another micro task that will happen after the above one
+  setTimeout(() => {
+    t.is(app.messages.length, 1);
+    t.is(app.messages[0], 'test message from mock server', 'we have subbed our websocket backend');
+    mockServer.stop(t.done);
+  }, 100);
+});
