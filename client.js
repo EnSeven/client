9cf02ce11502930e1ab03e4874ce170eeb9e3514
Brandon Haynes <brandon@Brandons-MBP-2.localdomain>
2018-12-01 18:23:50 -0800

    mob programming and troubleshooting output Saturday

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 'use strict';
 const io = require('socket.io-client');
-const superagent = require('superagent');
+// const superagent = require('superagent');
 const prompt = require('prompt');
 const socket = io.connect('https://enseven-game-engine.herokuapp.com');
 const readline = require('readline');
@@ -13,124 +13,98 @@ const rl = readline.createInterface({
 });
 
 
-// Welcome user. Explain how to use app by setting the rl prompt, and then calling the prompt
-rl.setPrompt('Welcome to N7 Games. To begin type LOGIN to login to an existing account, or type CREATE to create a new account.');
-rl.prompt();
+function welcomePrompt() {
+  // Welcome user. Explain how to use app by setting the rl prompt, and then calling the prompt
+  rl.question('Welcome to N7 Games. To begin type LOGIN to login to an existing account, or type CREATE to create a new account.', function(answer) {
+    firstQuestionPromptLoop(answer);
+  });
+  // firstQuestionPromptLoop();
+}
 
-//this needs to be wrapped in an invokable function, it's auto kicking to this before doing the welcome prompt
-rl.question('LOGIN or CREATE account? ', function(answer){
+function firstQuestionPromptLoop(answer) {
+  //this needs to be wrapped in an invokable function, it's auto kicking to this before doing the welcome prompt
   if(answer){
     switch(answer){
     case 'LOGIN':
-      // put login function / event here
+      login();
       break;
     case 'CREATE':
-      // put create account function / event here
+      // TODO: Check this out later
+      rl.close();
+      create();
+      // rl.resume();
       break;
-    //setting the default to kindly remind the user how to not cause errors and asking again
+      //setting the default to kindly remind the user how to not cause errors and asking again
     default:
       rl.setPrompt('LOGIN to login, CREATE to create new account. ');
-      rl.prompt();
-      // once we wrap this question in a function, callback this function to run again since the user did not type CREATE or LOGIN.
+      firstQuestionPromptLoop();
+        // once we wrap this question in a function, callback this function to run again since the user did not type CREATE or LOGIN.
     }
   }
-  rl.close();
-});
+  // console.log('brokeout of switch case');
+}
 
+
+function login() {
+  prompt.start();
+  const userSchema = {
+    properties: {
+      email: {
+        description: 'Enter your email address',
+        required: true,
+      },
+      username: {
+        description: 'Enter your username',
+        pattern: /^[a-zA-Z0-9]+$/,
+        message: 'Name must be only letters and numbers',
+        required: true,
+      },
+      password: {
+        description: 'Enter in your password',
+        hidden: true,
+        replace: '*',
+        required: true,
+      },
+    },
+  };
+  prompt.get(userSchema, function (err, result) {
+    socket.emit('signin', result);
+  });
+}
+
+function create() {
+  prompt.start();
+  const userSchema = {
+    properties: {
+      email: {
+        description: 'Enter your email address',
+        required: true,
+      },
+      username: {
+        description: 'Enter your username',
+        pattern: /^[a-zA-Z0-9]+$/,
+        message: 'Name must be only letters and numbers',
+        required: true,
+      },
+      password: {
+        description: 'Enter in your password',
+        hidden: true,
+        replace: '*',
+        required: true,
+      },
+    },
+  };
+  // Get two properties from the user: email, password
+  prompt.get(userSchema, function (err, result) {
+    // socket.emit('signup', result);
+  });
+}
 
 // on connection start the prompt module and collect login / account based on the userSchema (we can and should keep userSchema outside of the function later on)
 socket.on('connected', payload => {
-  function create() {
-    prompt.start();
-    const userSchema = {
-      properties: {
-        username: {
-          description: 'Enter your username',
-          pattern: /^[a-zA-Z0-9]+$/,
-          message: 'Name must be only letters and numbers',
-          required: true,
-        },
-        password: {
-          description: 'Enter in your password',
-          hidden: true,
-          replace: '*',
-          required: true,
-        },
-      },
-    };
-  
-    // Get two properties from the user: email, password
-    prompt.get(userSchema, function (err, result) {
-      // Log the results.
-      // result should be sent to API
-      // console.log('results', JSON.stringify(result));
-      console.log('Command-line input received:');
-      console.log('username: ' + result.username);
-      console.log('password: ' + result.password);
-  
-      let newResult = JSON.stringify(result);
-      superagent.post('https://enseven-api-service.herokuapp.com/signup')
-        .send(newResult)
-        .set('Content-Type', 'application/json')
-        .then(data =>{
-          // console.log('user data test', data);
-          socket.emit('join', data);
-          console.log('you have joined the game');
-        })
-        .catch(error => {
-          // console.log(error);
-          if (error) {
-            console.log('This username already exists with a different password. Please try again.');
-            login();
-          }
-        });
-    });
-  }
-  create();
-
-  function login() {
-    prompt.start();
-    const userSchema = {
-      properties: {
-        username: {
-          description: 'Enter your username',
-          pattern: /^[a-zA-Z0-9]+$/,
-          message: 'Name must be only letters and numbers',
-          required: true,
-        },
-        password: {
-          description: 'Enter in your password',
-          hidden: true,
-          replace: '*',
-          required: true,
-        },
-      },
-    };
-
-    prompt.get(userSchema, function (err, result) {
-      console.log('Command-line input received:');
-      console.log('username: ' + result.username);
-      console.log('password: ' + result.password);
-      // let newResultUser = JSON.stringify(result.username);
-      // let newResultPass = JSON.stringify(result.password);
-      superagent.post(`https://enseven-api-service.herokuapp.com/signin`)
-        .auth(result.username, result.password)
-        .set('Content-Type', 'application/json')
-        .then(data =>{
-          socket.emit('join', data);
-          console.log('you have joined the game');
-        })
-        .catch(error => {
-        // console.log(error);
-          if (error) {
-            console.log('Wrong Username/Password Combination. Try again.');
-            // console.log(error);
-            login();
-          }
-        });
-    });
-  }
+  // console.log('payload', payload);
 });
 
 // sending to sender-client only
+welcomePrompt();
 socket.emit('start');
