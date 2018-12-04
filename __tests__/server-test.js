6fc34fd20f9b5a3d807d9c6cb55733a015d9870b
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-12-03 21:46:41 -0800

    Got the new functionality working

--- __tests__/server-test.js
+++ __tests__/server-test.js
@@ -1,25 +1,81 @@
 'use strict';
 
-import supertest from 'supertest';
-import express from 'express';
-import http from 'http';
+const io = require('socket.io-client');
+const http = require('http');
+const ioBack = require('socket.io');
 
-// Ashley was having issues with open handles not closing with JEST so after some research, she found a temporary test that would pass on a forum. We will not be using this test going forward - this is just a placeholder for now. Here is the source for that: https://github.com/facebook/jest/issues/6907
-describe('demo test to keep the open handles away', () => {
-    let app, server;
+let socket;
+let httpServer;
+let httpServerAddr;
+let ioServer;
 
-    beforeAll(done => {
-        app = new express();
-        server = http.createServer(app);
-        server.listen(done);
+/**
+ * Setup WS & HTTP servers
+ */
+beforeAll((done) => {
+  httpServer = http.createServer().listen();
+  httpServerAddr = httpServer.listen().address();
+  ioServer = ioBack(httpServer);
+  done();
+});
+  
+/**
+   *  Cleanup WS & HTTP servers
+   */
+afterAll((done) => {
+  ioServer.close();
+  httpServer.close();
+  done();
+});
+  
+/**
+   * Run before each test
+   */
+beforeEach((done) => {
+  // Setup
+  // Do not hardcode server port and address, square brackets are used for IPv6
+  socket = io.connect(`http://[${httpServerAddr.address}]:${httpServerAddr.port}`, {
+    'reconnection delay': 0,
+    'reopen delay': 0,
+    'force new connection': true,
+    transports: ['websocket'],
+  });
+  socket.on('connect', () => {
+    done();
+  });
+});
+  
+/**
+   * Run after each test
+   */
+afterEach((done) => {
+  // Cleanup
+  if (socket.connected) {
+    socket.disconnect();
+  }
+  done();
+});
+
+describe('basic socket.io example', () => {
+  test('should communicate', (done) => {
+    // once connected, emit Hello World
+    ioServer.emit('echo', 'Hello World');
+    socket.once('echo', (message) => {
+      // Check that the message matches
+      expect(message).toBe('Hello World');
+      done();
     });
-
-    afterAll(done => {
-        server.close(done);
+    ioServer.on('connection', (mySocket) => {
+      expect(mySocket).toBeDefined();
     });
-
-    it('returns 404 when sent to route that does not exist', async () => {
-        const response = await supertest(app).get('/foobar');
-        expect(response.status).toBe(404);
-    });
+  });
+  test('should communicate with waiting for socket.io handshakes', (done) => {
+    // Emit sth from Client do Server
+    socket.emit('examlpe', 'some messages');
+    // Use timeout to wait for socket.io server handshakes
+    setTimeout(() => {
+      // Put your server side expect() here
+      done();
+    }, 50);
+  });
 });
--- client.js
+++ client.js
@@ -1,19 +1,23 @@
 'use strict';
 const io = require('socket.io-client');
 const prompt = require('prompt');
-const socket = io.connect('https://enseven-game-engine.herokuapp.com');
+const socket = io.connect('http://172.16.5.198:4040');
+// const socket = io.connect('https://enseven-game-engine.herokuapp.com');
+// const socket = io.connect('https://cdk-socket-io-test.herokuapp.com/');
+
 const readline = require('readline');
+// const players = io.of('/players');
 
 //setting readline to read from standard (keyboard) input and output streams. also setting a timeout limit if no input is received for any readline prompts / inputs.
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  escapeCodeTimeout: 30000,
+  escapeCodeTimeout: 20000,
 });
 
 function welcomePrompt() {
   // Welcome user. Explain how to use app by setting the rl prompt, and then calling the prompt
-  rl.question('Welcome to N7 Games. To begin type LOGIN to login to an existing account, or type CREATE to create a new account.  ', function(answer) {
+  rl.question('Welcome to N7 Games. To begin, type LOGIN to login to an existing account, or type CREATE to create a new account.  ', function(answer) {
     firstQuestionPromptLoop(answer);
   });
 }
@@ -24,22 +28,20 @@ function firstQuestionPromptLoop(answer) {
     switch(answer){
     case 'LOGIN':
       rl.close();
-      login();
+      signin();
       break;
     case 'CREATE':
       // TODO: Check out the rl.close later
       rl.close();
-      create();
+      signup();
       break;
-      //setting the default to kindly remind the user how to not cause errors and asking again
     default:
       welcomePrompt();
-        // once we wrap this question in a function, callback this function to run again since the user did not type CREATE or LOGIN.
     }
   }
 }
 
-function login() {
+function signin() {
   prompt.start();
   const userSchema = {
     properties: {
@@ -63,13 +65,11 @@ function login() {
   };
   
   prompt.get(userSchema, function (err, result) {
-    console.log(result);
     socket.emit('sign-in', result);
-    console.log('`Player ${socket.id} has signed in`');
   });
 }
 
-function create() {
+function signup() {
   prompt.start();
   const userSchema = {
     properties: {
@@ -78,13 +78,13 @@ function create() {
         required: true,
       },
       username: {
-        description: 'Enter your username',
+        description: 'Create a username',
         pattern: /^[a-zA-Z0-9]+$/,
         message: 'Name must be only letters and numbers',
         required: true,
       },
       password: {
-        description: 'Enter in your password',
+        description: 'Create a password',
         hidden: true,
         replace: '*',
         required: true,
@@ -93,17 +93,51 @@ function create() {
   };
   
   prompt.get(userSchema, function (err, result) {
-    console.log('result', result);
-    socket.emit('sign-up', result);
-    console.log('`Player ${socket.id} has signed up`');
+    socket.emit('sign-in', result);
+    // rl.pause();
+    // rl.close();
   });
 }
 
-// On connection start the prompt module and collect login / account based on the userSchema (we can and should keep userSchema outside of the function later on)
+// username comes with this
+socket.on('signed-in-newuser', payload => {
+  let user = payload;
+  socket.emit('join', user);
+  console.log('signed in new user');
+});
+socket.on('player1-joined', payload => {
+  console.log('player1 joined', payload);
+});
+socket.on('player2-joined', payload => {
+  console.log('player2 joined', payload);
+});
+// we can broadcast 
+// PLAY Is what will start the game - need two of these in order for it to begin 
+
+socket.emit('play');
+
+// socket.on('input-request')
+//     socket.emit('input', 'LETTER')
+
+// socket.on('won')
+
+// socket.on('lost')
+
+// socket.on('quit-game')
+//   socket.on('confirm-quit') // BOTH HAVE TO CONFIRM 
+//       socket.emit('quit-confirmed')
+
+//   socket.on('end', 'THIS QUITS COMPLETELY')
+
+//   socket.emit('get-stats')
+
+//   socket.on('stats', 'SHOW THE STATS')
+
+
+// Sending to game engine to START the game 
+socket.emit('start');
+
 socket.on('connected', payload => {
-  console.log(`Player ID ${socket.id} connected`);
+  console.log(payload);
   welcomePrompt();
 });
-
-// Sending to sender-client only
-socket.emit('start');
