1e931863b3b3adb4fa1f4d774d62e55d59c3a6b2
Mike K <m.kermes89@gmail.com>
2018-12-07 22:20:01 -0800

    updated client.js with working game-server

--- client.js
+++ client.js
@@ -1,36 +1,22 @@
 'use strict';
 const io = require('socket.io-client');
 const prompt = require('prompt');
-const socket = io.connect('http://172.16.5.198:4040');
+const socket = io.connect('http://172.16.5.198:4039');
+// const client = require('../Game-Engine/wordWizard/remoteClient.js');
 // const socket = io.connect('https://enseven-game-engine.herokuapp.com');
 // const socket = io.connect('https://cdk-socket-io-test.herokuapp.com/');
-
+let x = undefined;
 const readline = require('readline');
-const util = require('util');
-
-
 // const players = io.of('/players');
 
 //setting readline to read from standard (keyboard) input and output streams. also setting a timeout limit if no input is received for any readline prompts / inputs.
-//completer will allow users to tab auto complete anything that is listed in the completions array (all string get split into array). later commands or completer words can have functionality tied to them.
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   escapeCodeTimeout: 20000,
-  completer: (line, callback) => {
-    const completions = '!help !quit !q !exit CREATE LOGIN'.split(' ');
-    const hits = completions.filter((c) => c.startsWith(line));
-
-    setTimeout(
-      () => callback(null, [hits.length ? hits : completions, line]),
-      500,
-    );
-  },
 });
 
 function welcomePrompt() {
-  console.log('When prompted guess a letter to solve the hangman.\n Additional commands: !exit or !quit to stop playing, !stats to receive your game stats.')
   // Welcome user. Explain how to use app by setting the rl prompt, and then calling the prompt
   rl.question('Welcome to N7 Games. To begin, type LOGIN to login to an existing account, or type CREATE to create a new account.  ', function(answer) {
     firstQuestionPromptLoop(answer);
@@ -112,34 +98,56 @@ function signup() {
   });
 }
 
-//print help to user when !help is typed
-socket.on('!help', ()=> {
-  console.log('When prompted guess a letter to solve the hangman.\n Additional commands: !exit or !quit to stop playing, !stats to receive your game stats.')
-});
-
 // username comes with this
 socket.on('signed-in-newuser', payload => {
   let user = payload;
   socket.emit('join', user);
   console.log('signed in new user');
 });
-socket.on('player1-joined', payload => {
-  console.log('player1 joined', payload);
+socket.on('signed-in-user', payload => {
+  let user = payload;
+  socket.emit('join', user);
+  console.log('signed in returning user');
 });
 
+socket.on('player1-joined', payload => {
+  
+  console.log('player1 joined');
+  // console.log(payload);
+  socket.emit('play');
+});
+socket.on('input-request', (word) => {
+  console.log(word.category);
+  console.log(word.string);
+  prompt.start();
+  const input = {
+    properties: {
+      letter: {
+        description: 'Enter a letter',
+        required: true,
+      },
+    },
+  };
+  
+  prompt.get(input, function (err, result) {
+    socket.emit('input', result.letter);
+  });
+  
+});
+// socket.on('input-request', output => {
+//   client.promptInquirer(client.gameState);
+// });
+
 socket.on('player2-joined', payload => {
-  console.log('player2 joined', payload);
+  console.log('player2 joined');
+  console.log(payload);
   socket.emit('play');
 });
 
-socket.on('input-request-p1', () => {
-  socket.emit('input-p1', userInput);
-  console.log('sending player 1 letter');
-});
 
-socket.on('input-request-p2', () => {
-  socket.emit('input-p2', userInput);
-  console.log('sending player 2 letter');
+socket.on('results', results => {
+  console.log('results:', results);
+  socket.emit('play');
 });
 
 socket.on('won', () => {
@@ -150,16 +158,19 @@ socket.on('lost', () => {
   console.log('You have lost!');
 });
 
+
 // socket.on('quit-game')
 //   socket.on('confirm-quit') // BOTH HAVE TO CONFIRM 
 //       socket.emit('quit-confirmed')
 
 //   socket.on('end', 'THIS QUITS COMPLETELY')
 
-socket.emit('get-stats')
+socket.emit('get-stats');
 
 //   socket.on('stats', 'SHOW THE STATS')
-
+socket.on('output', (output) => {
+  console.log(output);
+});
 
 // Sending to game engine to START the game 
 socket.emit('start');
